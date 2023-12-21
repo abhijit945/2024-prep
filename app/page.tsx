e731ef4f66f21b327ccb34ee60c225def65bbe18
Abhijit Rao <abhijit945@gmail.com>
2023-12-21 14:34:14 -0600

    chore: remove package-lock

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export const dynamic = "force-dynamic";
 export default function Home() {
     return (
         <main className="relative flex min-h-screen flex-col space-y-2">
-            <h1 className="shadow-lg shadow-cyan-500/40 bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 p-6 m-2 rounded-lg flex items-center sticky top-0 z-20 transition-all text-4xl">
+            <h1 className="shadow-lg shadow-cyan-500/40 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 px-6 py-12 m-1 rounded-lg flex items-center sticky top-0 z-20 transition-all text-5xl">
                 Preparation 2024
             </h1>
             <Suspense fallback={<TablePlaceholder />}>
