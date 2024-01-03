fbfae2ee1c28fe280b9fa1a8c1dd9456fb29d6ac
Abhijit Rao <abhijit945@gmail.com>
2024-01-02 20:09:28 -0600

    chore: update icons for status

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import { AlgoExpertTable } from "@/components/algoExpertTable";
-import { LeetcodeTable } from "@/components/leetcodeTable";
-import { TablePlaceholder } from "@/components/table-placeholder";
-import { Suspense } from "react";
+import { AlgoExpertTable } from '@/components/algoExpertTable';
+import { LeetcodeTable } from '@/components/leetcodeTable';
+import { TablePlaceholder } from '@/components/table-placeholder';
+import { Suspense } from 'react';
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export default function Home() {
     return (
-        <main className="relative flex min-h-screen flex-col space-y-2">
-            <h1 className="shadow-lg shadow-cyan-500/40 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 px-6 py-12 m-1 rounded-lg flex items-center sticky top-0 z-20 transition-all text-5xl justify-center md:justify-start">
+        <main className='relative flex min-h-screen flex-col space-y-2'>
+            <h1 className='shadow-lg shadow-cyan-500/40 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 px-6 py-12 m-1 rounded-lg flex items-center sticky top-0 z-20 transition-all text-3xl justify-center md:justify-start'>
                 The Grind
             </h1>
             <Suspense fallback={<TablePlaceholder />}>
