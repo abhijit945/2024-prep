faadf914ed1c8a8afe92f21e1f4750f1bc353e1f
Abhijit Rao <abhijit945@gmail.com>
2024-01-05 15:11:07 -0600

    chore: update brand icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { AlgoExpertTable } from '@/components/algoExpertTable';
 import { LeetcodeTable } from '@/components/leetcodeTable';
 import { TablePlaceholder } from '@/components/table-placeholder';
+import Image from 'next/image';
 import { Suspense } from 'react';
 
 export const dynamic = 'force-dynamic';
@@ -8,7 +9,8 @@ export const dynamic = 'force-dynamic';
 export default function Home() {
     return (
         <main className='relative flex min-h-screen flex-col space-y-2'>
-            <h1 className='shadow-lg shadow-cyan-500/40 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 px-6 py-12 m-1 rounded-lg flex items-center sticky top-0 z-20 transition-all text-3xl justify-center md:justify-start'>
+            <h1 className='shadow-lg gap-4 shadow-cyan-500/40 bg-gradient-to-r from-cyan-500 via-purple-500 to-pink-500 px-6 py-12 m-1 rounded-lg flex items-center sticky top-0 z-20 transition-all text-3xl justify-center md:justify-start'>
+                <Image height={96} width={96} src='/brand.png' alt='brand' />
                 The Grind
             </h1>
             <Suspense fallback={<TablePlaceholder />}>
